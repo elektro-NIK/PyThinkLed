e0b0b5d6c032abbec6fbfecd3e1b99ed4c98fb89
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-01-18 00:06:30 +0200

    Add __main__

--- main.py
+++ main.py
@@ -36,18 +36,18 @@ def disk_led(dev):
         else:
             sleep(0.1)
 
-
-with open(diskstat, 'r') as f:
-    lines = f.read().split('\n')
-    hdd_list = [i.split()[2] for i in lines[:-1]]
-parser = argparse.ArgumentParser(description='Alternative realization ThinkPad LED functional')
-group = parser.add_mutually_exclusive_group()
-group.add_argument('--hdd', help='Use hdd indicator')
-group.add_argument('--hddlist', help='List of possible HDD', action="store_true")
-args = parser.parse_args()
-if args.hdd in hdd_list:
-    disk_led(args.hdd)
-if args.hddlist:
-    for i in hdd_list[:-1]:
-        print(i, end='\t')
-    print(hdd_list[-1])
+if __name__ == "__main__":
+    with open(diskstat, 'r') as f:
+        lines = f.read().split('\n')
+        hdd_list = [i.split()[2] for i in lines[:-1]]
+    parser = argparse.ArgumentParser(description='Alternative realization ThinkPad LED functional')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--hdd', help='Use hdd indicator')
+    group.add_argument('--hddlist', help='List of possible HDD', action="store_true")
+    args = parser.parse_args()
+    if args.hdd in hdd_list:
+        disk_led(args.hdd)
+    if args.hddlist:
+        for i in hdd_list[:-1]:
+            print(i, end='\t')
+        print(hdd_list[-1])
